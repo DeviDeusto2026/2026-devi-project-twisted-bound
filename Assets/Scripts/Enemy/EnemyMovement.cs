040a3f099219b174f5153f1dec633e5f87977bf6
JulenMendez <j.mendez@opendeusto.es>
2026-04-24 21:21:51 +0200

    Enemy basic movement created

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public GameObject target;
+    public GameObject player1;
+    public GameObject player2;
+
+    public float moveSpeed;
+    public float rotationSpeed;
+    
+    private Rigidbody rb;
+    private float diseredAngle;
+    
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        this.rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        SearchTarget();
+        Rotate();
+        Move();
+    }
+
+
+
+    private void SearchTarget()
+    {
+        float distanceToP1 = (this.transform.position - player1.transform.position).magnitude;
+        float distanceToP2 = (this.transform.position - player2.transform.position).magnitude;
+
+        if (distanceToP1 < distanceToP2)
+        {
+            target = player1;
+        }
+        else
+        {
+            target = player2;
+        }
+    }
+
+
+    private void Rotate()
+    {
+        Vector3 targetDirection = (target.transform.position - this.transform.position).normalized;
+
+        if (this.transform.forward == targetDirection) return;
+
+        float angle = Vector3.SignedAngle(this.transform.forward, targetDirection, Vector3.up);
+        diseredAngle = Mathf.Abs(angle);
+        float maxAngle = Time.fixedDeltaTime * rotationSpeed;
+
+        if (Mathf.Abs(angle) > maxAngle)
+        {
+            angle = Mathf.Sign(angle) * maxAngle;
+        }
+
+        this.transform.Rotate(Vector3.up, angle);
+    }
+
+    private void Move()
+    {
+        float speedModifier = 1 / ((diseredAngle / 36) + 1);
+        rb.linearVelocity = this.transform.forward * moveSpeed * speedModifier;
+        
+    }
+}
