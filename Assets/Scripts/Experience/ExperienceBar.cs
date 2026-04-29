0d6106a8984d1dbd6ae0f21d44db7282fb757b64
IbaiMC-Deusto <ibai.mellado@opendeusto.es>
2026-04-29 10:50:21 +0200

    ExperienceBar added
    Just the bar, not the pool of objects when leveling up

---
+++ Assets/Scripts/Experience/ExperienceBar.cs
@@ -0,0 +1,44 @@
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+
+public class Experience : MonoBehaviour
+{
+    private float experience = 0;
+    private float level = 1;
+
+    private const float constA = 8;
+    private const float constB = -5;
+    private readonly float constC = Mathf.Exp((1 - constB) / constA);
+
+    public void AddExperience(float experience)
+    {
+        SetExperience(this.GetExperience() + experience);
+    }
+
+    private float GetExperience()
+    {
+        return experience;
+    }
+    private void SetExperience(float experience)
+    {
+        this.experience = experience;
+        CheckLevel();
+    }
+
+    private void CheckLevel()
+    {
+        float newLevel = Mathf.Max(Mathf.Floor(constA * Mathf.Log10(experience + constC) + constB),1);
+
+        if (newLevel < level) return;
+
+        LevelUp();
+    }
+    private void LevelUp()
+    {
+        level++;
+
+        //Enseñar habilidades y que escogan una. Devolver nombre
+    }
+
+
+}
