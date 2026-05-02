9ed34ec3ced66ac49e8d7ac501c65d183f3ca4cf
IbaiMC-Deusto <ibai.mellado@opendeusto.es>
2026-05-02 11:38:50 +0200

    Created Item class

---
+++ Assets/Scripts/Player/Items/Item.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    string itemName;
+    Effect.StatEffect stat;
+    int level;
+    float power;
+
+    
+    public Effect.StatEffect GetStat()
+    {
+        return stat;
+    }
+    public float GetPower()
+    {
+        return level * power;
+    }
+
+    public void SetLevel(int level)
+    {
+        this.level = level;
+    }
+
+    public void LevelUp()
+    {
+        this.level = this.GetLevel() + 1;
+    }
+
+    public int GetLevel()
+    {
+        return this.level;
+    }
+
+    public string GetName()
+    {
+        return this.itemName;
+    }
+}
