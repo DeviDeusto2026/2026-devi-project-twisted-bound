f74f98ab96ad6762eb8a8e98dc0581e53cc6e94d
IbaiMC-Deusto <ibai.mellado@opendeusto.es>
2026-06-01 00:15:07 +0200

    changed experience orb color

--- Assets/Scripts/Experience/ExperienceOrb.cs
+++ Assets/Scripts/Experience/ExperienceOrb.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class ExperienceOrb : MonoBehaviour
 {
     [SerializeField] private float experience;
-
+    private void Start()
+    {
+        this.GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
     public void SetExperience(float experience)
     {
         this.experience = experience;
